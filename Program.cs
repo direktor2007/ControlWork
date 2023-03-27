66af9e45a0bf366ba241cda5f08850acd9b8d2ea
direktor2007 <>
2023-03-27 23:11:08 +0700

    added gitignor

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+string[] array1 = new string[3] { "Russia", "Denmark", "Kazan", };
+string[] array2 = new string[array1.Length];
+
+void ArraySecond(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3) ;
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+    }
+}
+
+ArraySecond(array1, array2);
+PrintArray(array2);
